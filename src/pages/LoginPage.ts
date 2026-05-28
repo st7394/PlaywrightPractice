f3198ba9e3e809d4ba552f799aeb5843044b7e33
st7394 <tripathi.saumitra7@gmail.com>
2026-05-28 08:26:10 +0530

    test

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from "@playwright/test";
 
 export class Loginpage {
   readonly page: Page;
@@ -6,22 +6,31 @@ export class Loginpage {
   readonly textboxPassword: Locator;
   readonly buttonLogin: Locator;
   readonly errorMessage: Locator;
+  readonly menuButton: Locator;
+  readonly logoutbutton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.textboxUsername = page.getByTestId('username');
-    this.textboxPassword = page.getByTestId('password');
-    this.buttonLogin = page.getByTestId('login-button');
-    this.errorMessage = page.getByTestId('error');
+    this.textboxUsername = page.getByTestId("username");
+    this.textboxPassword = page.getByTestId("password");
+    this.buttonLogin = page.getByTestId("login-button");
+    this.errorMessage = page.getByTestId("error");
+    this.logoutbutton = page.getByRole("link", { name: "Logout" });
+    this.menuButton = page.getByRole("button", { name: "Open Menu" });
   }
 
   async goto() {
-    await this.page.goto('/');
+    await this.page.goto("/");
   }
 
   async login(username: string, password: string) {
     await this.textboxUsername.fill(username);
     await this.textboxPassword.fill(password);
     await this.buttonLogin.click();
+  }
+
+  async successfulLogout() {
+    await this.menuButton.click();
+    await this.logoutbutton.click();
   }
 }
--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -13,5 +13,23 @@ test.describe("Login Tests", () => {
     await loginPage.login("standard_user", "secret_sauce");
     await expect(page).toHaveURL("/inventory.html");
     await expect(page.getByTestId("inventory-container")).toBeVisible();
+    await loginPage.successfulLogout();
+    // await loginPage.page.waitForTimeout(5000);
+  });
+
+  test("Login with invalid credentials", async ({ page }) => {
+    await loginPage.login("invalid_user", "invalid_password");
+    await expect(loginPage.errorMessage).toBeVisible();
+    await expect(loginPage.errorMessage).toHaveText(
+      "Epic sadface: Username and password do not match any user in this service",
+    );
+  });
+
+  test("Test with locked user", async ({ page }) => {
+    await loginPage.login("locked_out_user", "secret_sauce");
+    await expect(loginPage.errorMessage).toBeVisible();
+    await expect(loginPage.errorMessage).toHaveText(
+      "Epic sadface: Sorry, this user has been locked out.",
+    );
   });
 });
